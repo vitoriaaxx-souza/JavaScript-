ae217a2e37a0aefeb437c9b18d562c8ce35519be
vitoriaaxx-souza <fernanda.vitoria.rodrigues@escola.pr.gov.br>
2026-04-07 15:16:39 +0000

    Trabalho finalizado

--- README.md
+++
@@ -1,2 +0,0 @@
-# JavaScript-
-JavaScript | : algoritmos e ordenação 
---
+++ listaLivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+    {
+        titulo: "Vidas Secas",
+        preco: 600
+    },
+    {   
+        titulo: "A Vida de Alice",
+        preco: 100
+    },
+    {
+        titulo: "Ecos da Floresta",
+        preco: 69
+    }, 
+    {
+        titulo: "O Nome da Gente",
+        preco: 70
+    },
+    { 
+        titulo: "A Menina sem Nome",
+        preco: 50
+    },
+    {
+       titulo: "Chapéuzinho Amarelo",
+       preco: 90
+    }
+];
+
+module.exports = precosLivros;
