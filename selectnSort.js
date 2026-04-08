be1465d1b660269ba861f36381c350f506019b9c
vitoriaaxx-souza <fernanda.vitoria.rodrigues@escola.pr.gov.br>
2026-04-08 11:33:50 +0000

    trabalho 2 realizado

---
+++ selectnSort.js
@@ -0,0 +1,13 @@
+const livros = requre ('./listalivros');
+
+function menorValor(arrProdutos, posicaoInicial) {
+    let maisBarato = posicaoInicial;
+
+    for (let atual = posicaoInicial; atual < arrProduto.length; atual++) (
+        if (arrProdutos [atual].preco < arrProdutos [maisBarato].preco) {
+
+        }
+    )
+return maisBarato;
+}
+module.exports = menorValor; 
